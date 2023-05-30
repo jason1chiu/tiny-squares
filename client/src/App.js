952417ca74d71bdcd7364019c6644cb973c27419
cleclair71 <cassieleclair71@gmail.com>
2023-05-30 14:22:05 -0400

    App.js layout

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import styled from "styled-components";
+import LandingPage from './components/landing/App';
+
+// scroll to hash
+function useScrollToHash() {
+    const { hash } = useLocation();
+
+    useEffect(() => {
+        setTimeout(() => {
+          const id = hash.replace('#', '');
+          const element = document.getElementById(id);
+          if (element) element.scrollIntoView({ behavior: 'smooth' });
+        }, 0);
+      }, [hash]);
+    }
+    function App() {
+        return (
+          <Router>
+            <ScrollToHash />
+            <Container>
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                </Routes>
+            </Container>
+            </Router>
+        );
+    }
+    function ScrollToHash() {
+        useScrollToHash();
+        return null;
+    }
+    export default App;
+
+        const Container = styled.div``;
