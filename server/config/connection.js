f19a6150094f7ef71ab4a3aaa3e72cb23ea9eb25
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-05-31 16:54:33 -0400

    small change that shouldn't affect our local server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tinysquares", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/tinysquares", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
