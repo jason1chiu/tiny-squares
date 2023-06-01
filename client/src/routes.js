e9ef85933f955f9cd10687e0a5621ee960bb3bfb
cleclair71 <cassieleclair71@gmail.com>
2023-06-01 13:10:56 -0400

    routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,9 +8,15 @@ import {
   MdMenuBook
 } from "react-icons/md";
 
+//import Calendar from "./views/admin/calendar/Calendar.js";
+//import Create from "./views/admin/create/Create.js";
+//import Journals from "./views/admin/journals/Journals.js";
 import Dashboard from "./views/admin/dashboard/Dashboard.js";
 
 //auth 
+// import SignIn from "./views/admin/auth/SignIn.js";
+// import SignUp from "./views/admin/auth/SignUp.js";
+// import Landing from "./views/admin/auth/Landing.js";
 
 const routes = [
   {
@@ -40,6 +46,20 @@ const routes = [
   //     path: "/calendar",
   //     icon: <Icon as={MdOutlineCalendarMonth} width='20px' height='20px' color='inherit' />,
   //     component: Calendar,
+  // },
+  // {
+  //     name: "Signin",
+  //     layout: "/auth",
+  //     path: "/sign-in",
+  //     icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+  //     component: SignIn,
+  // },
+  // {
+  //     name: "Signup",
+  //     layout: "/auth",
+  //     path: "/sign-up",
+  //     icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+  //     component: SignUp,
   // },
 ];
 
