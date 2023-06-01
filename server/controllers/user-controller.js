52980dbac0099428bc1cbe09e1266018e2294b10
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-06-01 19:41:16 -0400

    templates for different controllers

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,14 +1,11 @@
-// import user model
 const { User } = require('../models');
-// import sign token function from auth
 const { signToken } = require('../utils/auth');
 
 module.exports = {
-  // get a single user by either their id or their username
   async getSingleUser({ user = null, params }, res) {
     const foundUser = await User.findOne({
       $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-    });
+    }).populate('journals');  // Populate the user's journals
 
     if (!foundUser) {
       return res.status(400).json({ message: 'Cannot find a user with this id!' });
@@ -16,7 +13,7 @@ module.exports = {
 
     res.json(foundUser);
   },
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+
   async createUser({ body }, res) {
     const user = await User.create(body);
 
@@ -26,8 +23,7 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
-  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-  // {body} is destructured req.body
+
   async login({ body }, res) {
     const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
     if (!user) {
@@ -42,27 +38,23 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-  // user comes from `req.user` created in the auth middleware function
-  async saveBook({ user, body }, res) {
-    console.log(user);
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $addToSet: { savedBooks: body } },
-        { new: true, runValidators: true }
-      );
-      return res.json(updatedUser);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json(err);
-    }
-  },
-  // remove a book from `savedBooks`
-  async deleteBook({ user, params }, res) {
+
+  async addJournal({ user, params }, res) {  // Add a journal to a user's 'journals' field
     const updatedUser = await User.findOneAndUpdate(
       { _id: user._id },
-      { $pull: { savedBooks: { bookId: params.bookId } } },
+      { $addToSet: { journals: params.journalId } },
+      { new: true, runValidators: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Couldn't find user with this id!" });
+    }
+    return res.json(updatedUser);
+  },
+
+  async removeJournal({ user, params }, res) {  // Remove a journal from 'journals'
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      { $pull: { journals: params.journalId } },
       { new: true }
     );
     if (!updatedUser) {
