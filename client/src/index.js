68904b84280dd38f4b98e79799f5dfad5b140a1f
cleclair71 <cassieleclair71@gmail.com>
2023-06-01 13:23:33 -0400

    index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import "assets/css/index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import AuthLayout from "layouts/auth";
+import AdminLayout from "layouts/admin";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "theme/theme";
+import { ThemeEditorProvider } from "@hypertheme-editor/chakra-ui";
+
+ReactDOM.render(
+    <ChakraProvider theme={theme}>
+      <React.StrictMode>
+        <ThemeEditorProvider>
+          <HashRouter>
+            <Switch>
+              <Route path={`/auth`} component={AuthLayout} />
+              <Route path={`/admin`} component={AdminLayout} />
+              <Redirect from='/' to='/admin' />
+              </Switch>
+        </HashRouter>
+      </ThemeEditorProvider>
+    </React.StrictMode>
+  </ChakraProvider>,
+  document.getElementById("root")
+);
