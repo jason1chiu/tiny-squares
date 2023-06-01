ddf6535d06f99ac3e0a8a3e4f9e38935a3af76c7
cass <76407605+cleclair71@users.noreply.github.com>
2023-05-31 20:37:26 -0400

    pie chart

---
+++ client/src/components/charts/Pie.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+class PieChart extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            chartData: [],
+            chartOptions: {},
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            chartData: this.props.chartData,
+            chartOptions: this.props.chartOptions,
+        });
+    }
+
+    render() {
+        return (
+            <div id="chart">
+                <ReactApexChart
+                    options={this.state.chartOptions}
+                    series={this.state.chartData}
+                    type="pie"
+                    width="100%"
+                />
+            </div>
+        );
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.chartData !== this.props.chartData) {
+            this.setState({
+                chartData: this.props.chartData,
+                chartOptions: this.props.chartOptions,
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        this.setState({
+            chartData: [],
+            chartOptions: {},
+        });
+    }
+}
+
+export default PieChart;
---
+++ client/src/variables/charts.js
@@ -0,0 +1,42 @@
+// Pie
+
+export const pieChartOptions = {
+    labels: ["Happy", "Sad", "Angry", "Neutral", "Fearful", "Disgusted", "Surprised"],
+    colors: ["#4318FF", "#6AD2FF", "#EFF4FB"],
+    chart: {
+        width: "50px",
+      },
+      states: {
+        hover: {
+          filter: {
+            type: "none",
+          },
+        },
+      },
+      legend: {
+        show: false,
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      hover: { mode: null },
+  plotOptions: {
+    donut: {
+      expandOnClick: false,
+      donut: {
+        labels: {
+          show: false,
+        },
+      },
+    },
+  },
+  fill: {
+    colors: ["#4318FF", "#6AD2FF", "#EFF4FB"],
+  },
+  tooltip: {
+    enabled: true,
+    theme: "dark",
+  },
+};
+
+export const pieChartData = [63, 25, 12];
