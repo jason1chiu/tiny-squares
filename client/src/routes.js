1122746705f971cadac41c8d16256fa7ad3175c9
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-06-01 11:46:30 -0400

    path to ./client/src/routes.js edited for Dashboard

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,46 +1,46 @@
 import React from "react";
 import { Icon } from "@chakra-ui/react";
 import {
-    MdHome,
-    MdLibraryAdd,
-    MdLock,
-    MdOutlineCalendarMonth,
-    MdMenuBook
-  } from "react-icons/md";
+  MdHome,
+  MdLibraryAdd,
+  MdLock,
+  MdOutlineCalendarMonth,
+  MdMenuBook
+} from "react-icons/md";
 
-  import Dashboard from "views/admin/dashboard/Dashboard.js";
+import Dashboard from "./views/admin/dashboard/Dashboard.js";
 
-  //auth 
+//auth 
 
-  const routes = [
-    {
-        name: "Dashboard",
-        layout: "/admin",
-        path: "/dashboard",
-        icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
-        component: Dashboard,
-    },
-    // {
-    //     name: "Journals",
-    //     layout: "/admin",
-    //     path: "/journals",
-    //     icon: <Icon as={MdMenuBook} width='20px' height='20px' color='inherit' />,
-    //     component: Journals,
-    // },
-    // {
-    //     name: "Create",
-    //     layout: "/admin",
-    //     path: "/create",
-    //     icon: <Icon as={MdLibraryAdd} width='20px' height='20px' color='inherit' />,
-    //     component: Create,
-    // },
-    // {
-    //     name: "Calendar",
-    //     layout: "/admin",
-    //     path: "/calendar",
-    //     icon: <Icon as={MdOutlineCalendarMonth} width='20px' height='20px' color='inherit' />,
-    //     component: Calendar,
-    // },
+const routes = [
+  {
+    name: "Dashboard",
+    layout: "/admin",
+    path: "/dashboard",
+    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    component: Dashboard,
+  },
+  // {
+  //     name: "Journals",
+  //     layout: "/admin",
+  //     path: "/journals",
+  //     icon: <Icon as={MdMenuBook} width='20px' height='20px' color='inherit' />,
+  //     component: Journals,
+  // },
+  // {
+  //     name: "Create",
+  //     layout: "/admin",
+  //     path: "/create",
+  //     icon: <Icon as={MdLibraryAdd} width='20px' height='20px' color='inherit' />,
+  //     component: Create,
+  // },
+  // {
+  //     name: "Calendar",
+  //     layout: "/admin",
+  //     path: "/calendar",
+  //     icon: <Icon as={MdOutlineCalendarMonth} width='20px' height='20px' color='inherit' />,
+  //     component: Calendar,
+  // },
 ];
 
 export default routes;
