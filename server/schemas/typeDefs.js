bd9d240aef10f7b0d6625feeff7477a0da403ea8
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-06-01 23:18:23 -0400

    edited develop branch

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,107 @@
+const { User, Journal, Entry } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if(context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('journals');
+      
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    journals: async (parent, args, context) => {
+      return Journal.find({}).populate('entries');
+    },
+    journal: async (parent, { _id }, context) => {
+      return Journal.findOne({ _id }).populate('entries');
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addJournal: async (parent, { input }, context) => {
+      if (context.user) {
+        const journal = await Journal.create(input);
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { journals: journal._id } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    removeJournal: async (parent, { journalId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { journals: journalId } },
+          { new: true }
+        );
+
+        await Journal.findByIdAndDelete(journalId);
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    addEntry: async (parent, { journalId, input }, context) => {
+      const entry = await Entry.create(input);
+      const updatedJournal = await Journal.findOneAndUpdate(
+        { _id: journalId },
+        { $addToSet: { entries: entry._id } },
+        { new: true }
+      );
+
+      return updatedJournal;
+    },
+
+    removeEntry: async (parent, { journalId, entryId }, context) => {
+      const updatedJournal = await Journal.findOneAndUpdate(
+        { _id: journalId },
+        { $pull: { entries: entryId } },
+        { new: true }
+      );
+
+      await Entry.findByIdAndDelete(entryId);
+
+      return updatedJournal;
+    }
+  }
+};
+
+module.exports = resolvers;
