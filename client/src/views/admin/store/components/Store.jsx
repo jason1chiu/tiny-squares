36b362c6eb68213492239ddd23878e9f0d14e748
Calvin Trieu <calvin.trieu@hotmail.com>
2023-06-02 02:19:38 -0400

    changed title

--- client/src/views/admin/store/components/Store.jsx
+++ client/src/views/admin/store/components/Store.jsx
@@ -7,7 +7,7 @@ import { productsArray } from '../js/ProductsStore'
 const Store = () => {
   return (
     <div className="parent">
-      <Center><Text fontSize='6xl'>Welcome to our store!</Text></Center>
+      <Center><Text fontSize='6xl'>Welcome to the Tiny Squares Store!</Text></Center>
 
      
       <Grid templateColumns='repeat(3, 1fr)' gap={3}>
