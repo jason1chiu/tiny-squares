1e63cc45fc06108e92fe8832303e37d8b5cb7969
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-06-02 00:41:19 -0400

    edited develop branch

--- client/src/views/admin/store/js/CartContext.js
+++ client/src/views/admin/store/js/CartContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState} from "react";
-import {productsArray, getProductData } from "./ProductsStore.js"
+import {productsArray, getProductData } from "./productsStore.js"
 
 export const CartContext = createContext({
     items: [],
