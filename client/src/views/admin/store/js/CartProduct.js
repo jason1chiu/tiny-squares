26355fd1e89ed1f24618c187bb5088b8c853e56c
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-06-02 00:08:32 -0400

    edited develop branch

--- client/src/views/admin/store/js/CartProduct.js
+++ client/src/views/admin/store/js/CartProduct.js
@@ -4,20 +4,20 @@ import { useContext } from 'react';
 import { getProductData } from './productsStore';
 
 function CartProduct(props) {
-    const cart = useContext(CartContext);
-    const id = props.id;
-    const quantity = props.quantity;
-    const productData = getProductData(id);
-    
-    return (
-        <>
-            <h3>{productData.title}</h3>
-            <p>{quantity} total</p>
-            <p>${(quantity * productData.price).toFixed(2)}</p>
-            <button onClick={() => cart.deleteFromCart(id)}>Remove</button>
-            <hr></hr>
-        </>
-    )
+  const cart = useContext(CartContext);
+  const id = props.id;
+  const quantity = props.quantity;
+  const productData = getProductData(id);
+
+  return (
+    <>
+      <h3>{productData.title}</h3>
+      <p>{quantity} total</p>
+      <p>${(quantity * productData.price).toFixed(2)}</p>
+      <button onClick={() => cart.deleteFromCart(id)}>Remove</button>
+      <hr></hr>
+    </>
+  )
 }
 
 export default CartProduct;
