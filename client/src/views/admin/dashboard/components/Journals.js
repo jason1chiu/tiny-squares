8857eeff2029492f82d2f8d94791d6c00191b06c
lmintsys <lubmin23@gmail.com>
2023-06-03 16:15:11 -0400

    connecting Journals from back-end to dashboard

--- client/src/views/admin/dashboard/components/Journals.js
+++ client/src/views/admin/dashboard/components/Journals.js
@@ -1,12 +1,17 @@
 import { Text, useColorModeValue } from "@chakra-ui/react";
-import Journal from "./Journal"
+import Journal from "./Journal";
 import Journal1 from "assets/img/purple.jpg";
 import Journal2 from "assets/img/purple.jpg";
 import Journal3 from "assets/img/purple.jpg";
 import Card from "components/card/card.js";
 import React from "react";
 
+import { GET_JOURNALS_DASHBOARD } from "utils/queries";
+import { useQuery } from "@apollo/client";
+
 export default function Journals(props) {
+  const { data } = useQuery(GET_JOURNALS_DASHBOARD);
+  console.log(data);
   const textColorPrimary = useColorModeValue("secondaryGray.900", "white");
   const textColorSecondary = "gray.400";
   const cardShadow = useColorModeValue(
@@ -17,38 +22,39 @@ export default function Journals(props) {
     <Card mb={{ base: "0px", "2xl": "20px" }}>
       <Text
         color={textColorPrimary}
-        fontWeight='bold'
-        fontSize='2xl'
-        mt='10px'
-        mb='4px'>
+        fontWeight="bold"
+        fontSize="2xl"
+        mt="10px"
+        mb="4px"
+      >
         All Journals
       </Text>
-      <Text color={textColorSecondary} fontSize='md' me='26px' mb='40px'>
+      <Text color={textColorSecondary} fontSize="md" me="26px" mb="40px">
         Make edits or changes
       </Text>
       <Journal
         boxShadow={cardShadow}
-        mb='20px'
+        mb="20px"
         image={Journal1}
-        link='#'
-        ranking='1'
-        title='Journal 1'
+        link="#"
+        ranking="1"
+        title="Journal 1"
       />
       <Journal
         boxShadow={cardShadow}
-        mb='20px'
+        mb="20px"
         image={Journal2}
-        link='#'
-        ranking='2'
-        title='Journal 2'
+        link="#"
+        ranking="2"
+        title="Journal 2"
       />
       <Journal
         boxShadow={cardShadow}
-        mb='20px'
+        mb="20px"
         image={Journal3}
-        link='#'
-        ranking='3'
-        title='Journal 3'
+        link="#"
+        ranking="3"
+        title="Journal 3"
       />
     </Card>
   );
