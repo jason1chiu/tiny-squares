911ff5e73b3411fa2e3e72fcce356b3cb98297a7
cleclair71 <cassieleclair71@gmail.com>
2023-06-02 20:22:15 -0400

    modal addition

--- client/src/views/admin/create/components/NewCard.js
+++ client/src/views/admin/create/components/NewCard.js
@@ -1,19 +1,38 @@
 import React, { useState } from 'react';
-import { Box, Center, useColorModeValue, ScaleFade } from '@chakra-ui/react';
+import { Box, Center, useColorModeValue } from '@chakra-ui/react';
 import { MdAddCircle } from 'react-icons/md';
 import Card from "components/card/card";
+import NewJournalModal from "views/admin/create/components/NewJournalModal";
 
-function NewCard({ onAddJournal }) {
+function NewCard() {
+    const [isModalOpen, setModalOpen] = useState(false);
     const [hover, setHover] = useState(false);
     const iconColor = useColorModeValue('secondaryGray.600', 'secondaryGray.600');
     const iconHoverColor = useColorModeValue('brand.500', 'white');
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
+    const handleAddJournal = (journalName) => {
+        // Add logic here to make API call to backend
+        // to create new journal using the given journalName.
+
+        closeModal();
+    };
 
     return (
         <Card 
             p='20px'
             _hover={{
                 boxShadow: 'lg',
-            }}>
+            }}
+            onClick={openModal} // Open the modal when the card is clicked
+        >
             <Box
                 h='200px'
                 display='flex'
@@ -22,7 +41,6 @@ function NewCard({ onAddJournal }) {
                 borderRadius='md'
                 onMouseEnter={() => setHover(true)}
                 onMouseLeave={() => setHover(false)}
-                onClick={onAddJournal} // placeholder for the actual add new journal function
                 cursor='pointer'
                 transition='color 0.2s'
                 color={hover ? iconHoverColor : iconColor}
@@ -31,6 +49,7 @@ function NewCard({ onAddJournal }) {
                     <MdAddCircle size='150px' />
                 </Center>
             </Box>
+            <NewJournalModal isOpen={isModalOpen} onClose={closeModal} onSubmit={handleAddJournal} />
         </Card>
     );
 }
