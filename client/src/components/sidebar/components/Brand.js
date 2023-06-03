6fa2e7f9a654b4bd54bb3413500b7b6c599c2369
cleclair71 <cassieleclair71@gmail.com>
2023-06-03 00:55:48 -0400

    logo sidebar

--- client/src/components/sidebar/components/Brand.js
+++ client/src/components/sidebar/components/Brand.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
 
 import { HSeparator } from "components/seperator/Seperator";
-
+import Logo from "assets/img/ts.png"
 export function SidebarBrand() {
-
-    let logoColor = useColorModeValue("navy.700", "white");
+    const textColor = useColorModeValue("secondaryGray.900", "white");
+   
 
     return (
         <Flex align='center' direction='column'>
-            {/* LOGO */}
-            <HSeparator mb='20px' />
+            <img src={Logo} alt="logo" width="80px" height="80px"/>
+            <Text align='center' color={textColor} fontSize='2xl' ms='24px' fontWeight='700'>
+            TinySquares
+              </Text>
+            <HSeparator mb='20px' mt='20px' />
             </Flex>
     );
 }
