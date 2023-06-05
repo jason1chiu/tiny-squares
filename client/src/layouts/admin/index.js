a48182f0f1b10a6012b069279fb128c072a3e612
cass <76407605+cleclair71@users.noreply.github.com>
2023-06-05 19:45:48 -0400

    fixed sign in and sign up displaying in the sidebar

--- client/src/layouts/admin/index.js
+++ client/src/layouts/admin/index.js
@@ -1,11 +1,13 @@
 import { Portal, Box, useDisclosure } from "@chakra-ui/react";
-import routes from "../../routes.js";
+import routes, { sidebarRoutes } from "routes.js";
 import React, { useState } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Footer from "../../components/footer/FooterAdmin.js";
-import Navbar from "../../components/navbar/AdminNav.js";
-import Sidebar from "../../components/sidebar/Sidebar.js";
-import { SidebarContext } from "../../contexts/SidebarContext.js";
+import Footer from "components/footer/FooterAdmin.js";
+import Navbar from "components/navbar/AdminNav.js";
+import Sidebar from "components/sidebar/Sidebar.js";
+import { SidebarContext } from "contexts/SidebarContext.js";
+import SignIn from "views/auth/signIn";
+import SignUp from "views/auth/signUp";
 
 export default function Dashboard(props) {
   const { ...rest } = props;
@@ -16,7 +18,7 @@ export default function Dashboard(props) {
   const getRoute = (routes) => {
     return window.location.pathname !== "/admin/full-screen-maps"
   };
-
+ 
   const getActiveRoute = (routes) => {
     let activeRoute = "Dashboard";
     for (let i = 0; i < routes.length; i++) {
@@ -118,7 +120,7 @@ export default function Dashboard(props) {
           toggleSidebar,
           setToggleSidebar,
         }}>
-        <Sidebar routes={routes} display='none' {...rest} />
+        <Sidebar routes={sidebarRoutes} display='none' {...rest} />
         <Box
           float='right'
           minHeight='100vh'
@@ -153,8 +155,10 @@ export default function Dashboard(props) {
               minH='100vh'
               pt='50px'>
               <Switch>
-                {getRoutes(routes)}
-                <Redirect from='/' to='/admin/dashboard' />
+              {getRoutes(sidebarRoutes)}
+    <Route path='/auth/sign-in' component={SignIn} />
+    <Route path='/auth/sign-up' component={SignUp} />
+    <Redirect from='/' to='/admin/dashboard' />
               </Switch>
             </Box>
           ) : null}
