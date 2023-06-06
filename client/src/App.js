b809623ceb48407a0f6e93b3bc7cd9e66cf818a7
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-06-06 18:00:46 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "assets/css/index.css";
 
+
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
@@ -12,19 +13,27 @@ import CancelPage from "views/admin/cancelOrderPage/"
 import SuccessPage from "views/admin/successOrderPage"
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { useAuth } from 'contexts/auth.context';
+import { useCookies } from 'react-cookie';
 
 // Create an Apollo Client and specify the connection to your GraphQL API
 
 export default function App() {
 
   let { user } = useAuth();
+  let [ cookies ] = useCookies();
+  function getToken(user, cookies){  
+    let token = (user && user.token) || cookies.token;
+    return token ? `Bearer ${token}` : ""
+  }
+
   const client = new ApolloClient({
     uri: 'http://localhost:3001/graphql',
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({query: true, data: false}),
     headers: {
-      authorization: user ? `Bearer ${user.token}` : "",
+      authorization: getToken(user, cookies),
     },
   });
+
   const pageVariants = {
     initial: {
       opacity: 0,
