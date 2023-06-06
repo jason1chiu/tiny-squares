a0b6848c8200f96ad1e72b599b8a6ebf9da3c2a8
Calvin Trieu <calvin.trieu@hotmail.com>
2023-06-06 01:43:58 -0400

    legend add, delete, and edit all working

--- client/src/components/shared/calendar/components/legend/Legend.js
+++ client/src/components/shared/calendar/components/legend/Legend.js
@@ -17,7 +17,7 @@ const Legend = () => {
   const [updateLegend] = useMutation(UPDATE_LEGEND);
   const [deleteLegend] = useMutation(DELETE_LEGEND);
 
-  const { loading, error, data,} = useQuery(GET_LEGENDS, {
+  const { loading, error, data } = useQuery(GET_LEGENDS, {
     variables: { id: userId },
   });
 
@@ -26,64 +26,88 @@ const Legend = () => {
       setLegends(data.legends);
     }
   }, [data]);
-  // Initialized the first item in legend array as something blank
-  useState(() => {
-    setLegends([{ label: "Add Your Selection" }]);
-  }, []);
- 
 
-  const handleAddLegend = async (label, color) => {
+  const handleAddLegend = async () => {
     try {
-
       if (!userId) {
         console.error("User ID is missing or invalid.");
         return;
       }
 
-      const {data} = await createLegend({
-        variables: { label, color, userId},
+      const { data } = await createLegend({
+        variables: { label, color, userId },
       });
 
       const createdLegend = data.createLegend;
-       setLegends((prevLegends) => [...prevLegends, createdLegend]);
-      
-      console.log('Created Legend: ', data.createLegend);
+      setLegends((prevLegends) => [...prevLegends, createdLegend]);
+
+      console.log("Created Legend: ", data.createLegend);
     } catch (error) {
       console.error("Error creating legend: ", error);
     }
   };
 
-  const handleDeleteLegend = (legendId) => {
-  
-    console.log("Legend ID:", legendId);
-    deleteLegend({
-      variables: { id: legendId },
-      update: (cache) => {
-        // Update the cache after successful deletion
-        cache.modify({
-          fields: {
-            legends(existingLegends, { readField }) {
-              return existingLegends.filter(
-                (legendRef) => legendId !== readField("id", legendRef)
-              );
-            },
-          },
-        });
-      },
-    })
-      .then((response) => {
-        // Handle successful deletion
-        console.log("Legend deleted:", response);
-      
-      })
-      .catch((error) => {
-        // Handle error
-        console.error("Failed to delete legend:", error);
+  const handleUpdateLegend = async () => {
+    try {
+      if (selectedIndex === null) {
+        console.error("Selected index is missing or invalid.");
+        return;
+      }
+
+      const legendId = legends[selectedIndex].id;
+
+      const { data } = await updateLegend({
+        variables: { id: legendId, label, color },
       });
+
+      const updatedLegend = data.updateLegend;
+      setLegends((prevLegends) => {
+        const updatedLegends = [...prevLegends];
+        updatedLegends[selectedIndex] = updatedLegend;
+        return updatedLegends;
+      });
+
+      console.log("Updated Legend: ", data.updateLegend);
+      handleCancelEdit();
+    } catch (error) {
+      console.error("Error updating legend: ", error);
+    }
+  };
+
+  const handleDeleteLegend = async (legendId) => {
+    console.log("Legend ID:", legendId);
+    try {
+      await deleteLegend({
+        variables: { id: legendId },
+        update: (cache) => {
+          cache.modify({
+            fields: {
+              legends(existingLegends, { readField }) {
+                return existingLegends.filter(
+                  (legendRef) => legendId !== readField("id", legendRef)
+                );
+              },
+            },
+          });
+        },
+      });
+      console.log("Legend deleted:", legendId);
+    } catch (error) {
+      console.error("Failed to delete legend:", error);
+    }
   };
 
   const handleEditLegend = (index) => {
+    setSelectedIndex(index);
+    const selectedLegend = legends[index];
+    setLabel(selectedLegend.label);
+    setColor(selectedLegend.color);
+  };
 
+  const handleCancelEdit = () => {
+    setSelectedIndex(null);
+    setLabel("");
+    setColor("#000000");
   };
 
   if (loading) {
@@ -94,34 +118,77 @@ const Legend = () => {
     return <p>Error: {error.message}</p>;
   }
 
-
   return (
-    // <Card mt={4} mb={6} mx="auto" p={4} w="auto">
     <VStack spacing={2}>
-      <Heading size="sm" mb={2}>Legend</Heading>
+      <Heading size="sm" mb={2}>
+        Legend
+      </Heading>
       <HStack spacing={1}>
-        <Input size="sm" type="color" value={color} onChange={(e) => setColor(e.target.value)} />
-        <Input size="sm" type="text" placeholder="Label" value={label} onChange={(e) => setLabel(e.target.value)} />
-        {/* <Button size="xs" onClick={() => handleAddLegend(label, color, userId)}>{selectedIndex === null ? "Add" : "Update"}</Button> */}
-        <Button size="xs" onClick={() => handleAddLegend(label, color, userId)}>Add</Button>
+        <Input
+          size="sm"
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <Input
+          size="sm"
+          type="text"
+          placeholder="Label"
+          value={label}
+          onChange={(e) => setLabel(e.target.value)}
+        />
+        {selectedIndex === null ? (
+          <Button size="xs" onClick={handleAddLegend}>
+            Add
+          </Button>
+        ) : (
+          <>
+            <Button size="xs" onClick={handleUpdateLegend}>
+              Update
+            </Button>
+            <Button size="xs" onClick={handleCancelEdit}>
+              Cancel
+            </Button>
+          </>
+        )}
       </HStack>
 
       {legends.map((legend, index) => (
-        
         <HStack key={index} spacing={1}>
           {index !== 0 && (
             <>
-          <Box boxSize="1em" bgColor={legend.color} border="1px solid" borderColor="gray.200" />
-          <Text fontSize="sm">{legend.label}</Text>
-          <Button size="xs" onClick={() => handleEditLegend(index)}>Edit</Button>
-          <Button size="xs" onClick={() => handleDeleteLegend(legend.id)}>Delete</Button>
-          </>
+              <Box
+                boxSize="1em"
+                bgColor={legend.color}
+                border="1px solid"
+                borderColor="gray.200"
+              />
+              <Text fontSize="sm">{legend.label}</Text>
+              {selectedIndex === index ? (
+                <Button size="xs" onClick={() => handleEditLegend(index)}>
+                  Edit
+                </Button>
+              ) : (
+                <>
+                  <Button
+                    size="xs"
+                    onClick={() => handleEditLegend(index)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    size="xs"
+                    onClick={() => handleDeleteLegend(legend.id)}
+                  >
+                    Delete
+                  </Button>
+                </>
+              )}
+            </>
           )}
         </HStack>
       ))}
-
     </VStack>
-    // </Card>
   );
 };
 
