3390b4fb8f49bd7d5eeecf2ac10f6bf16a94d477
Calvin Trieu <calvin.trieu@hotmail.com>
2023-06-06 02:09:13 -0400

    Able to pull backend info for cells

--- client/src/components/shared/calendar/components/board/Cell.js
+++ client/src/components/shared/calendar/components/board/Cell.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
 import { Box, useDisclosure } from "@chakra-ui/react";
 import CellModal from "components/shared/calendar/components/modal/modal";
+import { GET_LEGENDS, GET_ME } from "utils/queries";
 
 const Cell = ({ day, month, color, note, onSave, legends }) => {
+  const userId = localStorage.getItem("user_id");
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const legendsQuery = useQuery(GET_LEGENDS, {
+    variables: {id: userId}
+  });
+  console.log("Legends data:", legendsQuery.data);
   const handleCellClick = () => {
     onOpen();
   };
@@ -26,7 +32,7 @@ const Cell = ({ day, month, color, note, onSave, legends }) => {
         cursor="pointer"
         onClick={handleCellClick}
       ></Box>
-      <CellModal isOpen={isOpen} onClose={onClose} onSave={handleSave} initialColor={color} initialNote={note} legends={legends} />
+      <CellModal isOpen={isOpen} onClose={onClose} onSave={handleSave} initialColor={color} initialNote={note} legends={legendsQuery.data ? legendsQuery.data.legends : []} />
     </>
   );
 };
