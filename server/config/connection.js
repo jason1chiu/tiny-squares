494c503ffbd310c7ef1e2471a6b1e71d66a3edde
Calvin Trieu <calvin.trieu@hotmail.com>
2023-06-07 21:02:56 -0400

    HEORKU

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/tinysquares", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log("Database Connected Successfully"))
