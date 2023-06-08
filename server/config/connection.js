ac12014d74df6f70d4bd9fd0c828eaa8189f35cf
Calvin Trieu <calvin.trieu@hotmail.com>
2023-06-07 20:10:49 -0400

    heorku

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/tinysquares", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(() => console.log("Database Connected Successfully"))
+.catch(err => console.log(err));
 
 module.exports = mongoose.connection;
