0875c7ab6c6a0c3d26bc5f2c9c916749e71badb3
cass <76407605+cleclair71@users.noreply.github.com>
2023-06-07 21:37:21 -0400

    styling

--- client/src/views/admin/journals/index.jsx
+++ client/src/views/admin/journals/index.jsx
@@ -58,17 +58,8 @@ export default function JournalPage() {
                 ms={{ base: "24px", md: "0px" }}
                 mt={{ base: "20px", md: "0px" }}
               >
-                {categories.map((category) => (
-                  <Link
-                    key={category}
-                    color={textColorBrand}
-                    fontWeight="500"
-                    me={{ base: "34px", md: "44px" }}
-                    to="#"
-                  >
-                    {category}
-                  </Link>
-                ))}
+               
+            
               </Flex>
             </Flex>
             <SimpleGrid columns={{ base: 1, md: 3 }} gap="20px">
