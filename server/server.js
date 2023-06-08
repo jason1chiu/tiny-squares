fab6cfbf7d6402bb3e8457d4fb641b08c45f73a9
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-06-07 23:08:34 -0400

    deploying

--- server/server.js
+++ server/server.js
@@ -48,8 +48,10 @@ app.post("/admin/store/checkout", async (req, res) => {
     line_items: lineItems,
     mode: 'payment',
     // *** When deploying to heroku, change url to https://your-app-name.herokuapp.com/success
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel"
+    // success_url: "http://localhost:3000/success",
+    // cancel_url: "http://localhost:3000/cancel"
+    success_url: "https://enigmatic-stream-88678.herokuapp.com/success",
+    cancel_url: "https://enigmatic-stream-88678.herokuapp.com/cancel"
   });
 
   res.send(JSON.stringify({
