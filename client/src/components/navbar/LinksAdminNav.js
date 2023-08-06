481cb7ad35b43c71a3d9935094c69dd17dbfe4f7
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-08-05 21:39:18 -0400

    believe to get user's profile pic to update

--- client/src/components/navbar/LinksAdminNav.js
+++ client/src/components/navbar/LinksAdminNav.js
@@ -3,23 +3,39 @@ import { MdShoppingCart, MdEdit, MdHelpOutline } from "react-icons/md";
 import { FaEthereum } from "react-icons/fa";
 import { useHistory } from "react-router-dom";
 import EditProfileModal from "components/modal/EditProfileModal";
-import { CartContext } from 'components/shared/store/js/CartContext'
+import { CartContext } from "components/shared/store/js/CartContext";
 import { useState, useContext } from "react";
 import { BsShop } from "react-icons/bs";
-import { CartModal } from 'components/shared/store/components/CartModal'
+import { CartModal } from "components/shared/store/components/CartModal";
 import { useCookies } from "react-cookie";
 // import Tutorial from "components/navbar/tutorial";
 
-import { motion } from 'framer-motion';
-import BuyOptionsModal from 'components/shared/store/components/BuyOptionsModal'
+import { motion } from "framer-motion";
+import BuyOptionsModal from "components/shared/store/components/BuyOptionsModal";
 
 // Chakra imports
-import { Avatar, Button, Flex, Icon, Link, Menu, MenuButton, MenuItem, MenuList, Text, Badge, useColorModeValue, useDisclosure, IconButton, Box } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Flex,
+  Icon,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  Badge,
+  useColorModeValue,
+  useDisclosure,
+  IconButton,
+  Box,
+} from "@chakra-ui/react";
 
 import PropTypes from "prop-types";
 
 // Apollow imports
-import { useMutation } from "@apollo/client"
+import { useMutation } from "@apollo/client";
 
 // File imports
 import routes from "routes";
@@ -35,15 +51,15 @@ export default function HeaderLinks(props) {
   const [logout] = useMutation(LOGOUT_USER);
   const history = useHistory();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  
+
   const MotionMenuList = motion(MenuList);
   const MotionMenuItem = motion(MenuItem);
   const menuVariants = {
     open: {
-      transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+      transition: { staggerChildren: 0.07, delayChildren: 0.2 },
     },
     closed: {
-      transition: { staggerChildren: 0.05, staggerDirection: -1 }
+      transition: { staggerChildren: 0.05, staggerDirection: -1 },
     },
   };
 
@@ -57,7 +73,7 @@ export default function HeaderLinks(props) {
       let email = user.user.email;
       await logout({ variables: { email } });
       setUser(false);
-      removeCookie('token');
+      removeCookie("token");
       localStorage.clear();
       setTimeout(() => history.push("/auth/sign-in"), 100);
     } catch (error) {
@@ -83,111 +99,133 @@ export default function HeaderLinks(props) {
 
   const cart = useContext(CartContext);
   const totalQuantity = cart.getTotalQuantity();
-  const { isOpen: buyOptionsModalIsOpen, onOpen: openBuyOptionsModal, onClose: closeBuyOptionsModal } = useDisclosure();
-  const { isOpen: cartModalIsOpen, onOpen: openCartModal, onClose: closeCartModal } = useDisclosure();
+  const {
+    isOpen: buyOptionsModalIsOpen,
+    onOpen: openBuyOptionsModal,
+    onClose: closeBuyOptionsModal,
+  } = useDisclosure();
+  const {
+    isOpen: cartModalIsOpen,
+    onOpen: openCartModal,
+    onClose: closeCartModal,
+  } = useDisclosure();
   // const [runTutorial, setRunTutorial] = useState(false);
   return (
-    user && user.user &&
-    <Flex
-      w={{ sm: "100%", md: "auto" }}
-      alignItems="center"
-      flexDirection="row"
-      bg={menuBg}
-      flexWrap={secondary ? { base: "wrap", md: "nowrap" } : "unset"}
-      p="10px"
-      borderRadius="30px"
-      boxShadow={shadow}
-    >
+    user &&
+    user.user && (
       <Flex
-        bg={ethBg}
-        display={secondary ? "flex" : "none"}
+        w={{ sm: "100%", md: "auto" }}
+        alignItems="center"
+        flexDirection="row"
+        bg={menuBg}
+        flexWrap={secondary ? { base: "wrap", md: "nowrap" } : "unset"}
+        p="10px"
         borderRadius="30px"
-        ms="auto"
-        p="6px"
-        align="center"
-        me="6px"
+        boxShadow={shadow}
       >
         <Flex
-          align="center"
-          justify="center"
-          bg={ethBox}
-          h="29px"
-          w="29px"
+          bg={ethBg}
+          display={secondary ? "flex" : "none"}
           borderRadius="30px"
-          me="7px"
+          ms="auto"
+          p="6px"
+          align="center"
+          me="6px"
         >
-          <Icon color={ethColor} w="9px" h="14px" as={FaEthereum} />
+          <Flex
+            align="center"
+            justify="center"
+            bg={ethBox}
+            h="29px"
+            w="29px"
+            borderRadius="30px"
+            me="7px"
+          >
+            <Icon color={ethColor} w="9px" h="14px" as={FaEthereum} />
+          </Flex>
         </Flex>
 
-      </Flex>
+        <SidebarResponsive routes={routes} />
 
-      <SidebarResponsive routes={routes} />
-
-      <Menu>
-        <MenuList
-          boxShadow={shadow}
-          p="20px"
-          borderRadius="20px"
-          bg={menuBg}
-          border="none"
-          mt="22px"
-          me={{ base: "30px", md: "unset" }}
-          minW={{ base: "unset", md: "400px", xl: "450px" }}
-          maxW={{ base: "360px", md: "unset" }}
-        >
-          <Flex flexDirection="column">
-            <MenuItem
-              _hover={{ bg: "none" }}
-              _focus={{ bg: "none" }}
-              px="0"
-              borderRadius="8px"
-              mb="10px"
-            >
-              <ItemContent info="infor" aName="Name" />
-            </MenuItem>
-            <MenuItem
-              _hover={{ bg: "none" }}
-              _focus={{ bg: "none" }}
-              px="0"
-              borderRadius="8px"
-              mb="10px"
-            >
-              <ItemContent info="infor" aName="name" />
-            </MenuItem>
-          </Flex>
-        </MenuList>
-      </Menu>
-      <Menu>
-        <IconButton
-          icon={(
-            <Box position="relative">
-              <MdShoppingCart size="24" tm="100px" />
-              <Badge colorScheme="purple" boxShadow='md' position="absolute" boxSize="20px" borderRadius="full" display="flex" alignItems="center" justifyContent="center" top="-18px" right="-10px" fontSize='.7em'>
-                {totalQuantity}
-              </Badge>
-            </Box>
-          )}
-          color={navbarIcon}
-          _hover={{ color: "secondaryGray.900" }} 
-          onClick={openCartModal}
-        />
-        <CartModal isOpen={cartModalIsOpen} onClose={closeCartModal} />
-      </Menu>
-      <Menu>
-        <IconButton
-          icon={(
-            <Box position="relative">
-              <BsShop size="24" tm="100px" />
-
-            </Box>
-          )}
-          color={navbarIcon}
-          _hover={{ color: "secondaryGray.900" }}
-          onClick={openBuyOptionsModal}
-        />
-        <BuyOptionsModal isOpen={buyOptionsModalIsOpen} onClose={closeBuyOptionsModal} />
-      </Menu>
-      {/* <Tutorial run={runTutorial} setRun={setRunTutorial} />
+        <Menu>
+          <MenuList
+            boxShadow={shadow}
+            p="20px"
+            borderRadius="20px"
+            bg={menuBg}
+            border="none"
+            mt="22px"
+            me={{ base: "30px", md: "unset" }}
+            minW={{ base: "unset", md: "400px", xl: "450px" }}
+            maxW={{ base: "360px", md: "unset" }}
+          >
+            <Flex flexDirection="column">
+              <MenuItem
+                _hover={{ bg: "none" }}
+                _focus={{ bg: "none" }}
+                px="0"
+                borderRadius="8px"
+                mb="10px"
+              >
+                <ItemContent info="infor" aName="Name" />
+              </MenuItem>
+              <MenuItem
+                _hover={{ bg: "none" }}
+                _focus={{ bg: "none" }}
+                px="0"
+                borderRadius="8px"
+                mb="10px"
+              >
+                <ItemContent info="infor" aName="name" />
+              </MenuItem>
+            </Flex>
+          </MenuList>
+        </Menu>
+        <Menu>
+          <IconButton
+            icon={
+              <Box position="relative">
+                <MdShoppingCart size="24" tm="100px" />
+                <Badge
+                  colorScheme="purple"
+                  boxShadow="md"
+                  position="absolute"
+                  boxSize="20px"
+                  borderRadius="full"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  top="-18px"
+                  right="-10px"
+                  fontSize=".7em"
+                >
+                  {totalQuantity}
+                </Badge>
+              </Box>
+            }
+            color={navbarIcon}
+            _hover={{ color: "secondaryGray.900" }}
+            onClick={openCartModal}
+          />
+          <CartModal isOpen={cartModalIsOpen} onClose={closeCartModal} />
+        </Menu>
+        <Menu>
+          <IconButton
+            icon={
+              <Box position="relative">
+                <BsShop size="24" tm="100px" />
+              </Box>
+            }
+            color={navbarIcon}
+            _hover={{ color: "secondaryGray.900" }}
+            onClick={openBuyOptionsModal}
+          />
+          <BuyOptionsModal
+            isOpen={buyOptionsModalIsOpen}
+            onClose={closeBuyOptionsModal}
+          />
+        </Menu>
+        {/* <Tutorial run={runTutorial} setRun={setRunTutorial} />
     <Menu>
         <IconButton
           icon={<MdHelpOutline size="24" />}
@@ -196,87 +234,86 @@ export default function HeaderLinks(props) {
           onClick={() => setRunTutorial(true)} // Start the tutorial when the icon is clicked
         />
     </Menu> */}
-      <Menu>
-        <MenuButton p="0px">
-
-          <Avatar
-            _hover={{ cursor: "pointer" }}
-            color="white"
-            name={user.user.username}
-            bg="#11047A"
-            size="sm"
-            w="40px"
-            h="40px"
-          />
-        </MenuButton>
-        <MotionMenuList
-          boxShadow={shadow}
-          p="0px"
-          mt="10px"
-          borderRadius="20px"
-          bg={menuBg}
-          border="none"
-          variants={menuVariants}
-          initial="closed"
-          animate={isOpen ? "open" : "closed"}
-        >
-          <Flex w="100%" mb="0px">
-            <Text
-              ps="20px"
-              pt="16px"
-              pb="10px"
-              w="100%"
-              borderBottom="1px solid"
-              borderColor={borderColor}
-              fontSize="sm"
-              fontWeight="700"
-              color={textColor}
-            >
-              👋&nbsp; Hey, {user.user.username}
-            </Text>
-          </Flex>
-
-          <Flex flexDirection="column" p="10px">
+        <Menu>
+          <MenuButton p="0px">
+            <Avatar
+              _hover={{ cursor: "pointer" }}
+              color="white"
+              name={user.user.username}
+              bg="#11047A"
+              size="sm"
+              w="40px"
+              h="40px"
+            />
+          </MenuButton>
+          <MotionMenuList
+            boxShadow={shadow}
+            p="0px"
+            mt="10px"
+            borderRadius="20px"
+            bg={menuBg}
+            border="none"
+            variants={menuVariants}
+            initial="closed"
+            animate={isOpen ? "open" : "closed"}
+          >
+            <Flex w="100%" mb="0px">
+              <Text
+                ps="20px"
+                pt="16px"
+                pb="10px"
+                w="100%"
+                borderBottom="1px solid"
+                borderColor={borderColor}
+                fontSize="sm"
+                fontWeight="700"
+                color={textColor}
+              >
+                👋&nbsp; Hey, {user.user.username}
+              </Text>
+            </Flex>
 
             <Flex flexDirection="column" p="10px">
-              <MotionMenuItem
-                _hover={{ bg: 'none' }}
-                _focus={{ bg: 'none' }}
-                color={textColor}
-                borderRadius="8px"
-                px="14px"
-                onClick={onOpen} // open the modal when this item is clicked
-                variants={menuItemVariants}
-              >
-                <Icon as={MdEdit} w={5} h={5} mr={2} /> Edit Profile
-                <EditProfileModal isOpen={isOpen} onClose={onClose} />
-              </MotionMenuItem>
+              <Flex flexDirection="column" p="10px">
+                <MotionMenuItem
+                  _hover={{ bg: "none" }}
+                  _focus={{ bg: "none" }}
+                  color={textColor}
+                  borderRadius="8px"
+                  px="14px"
+                  onClick={onOpen} // open the modal when this item is clicked
+                  variants={menuItemVariants}
+                >
+                  <Icon as={MdEdit} w={5} h={5} mr={2} /> Edit Profile
+                  <EditProfileModal isOpen={isOpen} onClose={onClose} />
+                </MotionMenuItem>
 
-              <MenuItem
-                _hover={{ bg: "none" }}
-                _focus={{ bg: "none" }}
-                color="red.400"
-                borderRadius="8px"
-                px="14px"
-              >
-                <Link w="100%" href="#">
-                  <Button
-                    w="100%"
-                    h="44px"
-                    variant="no-hover"
-                    color={textColor}
-                    bg="transparent"
-                    onClick={handleLogout}
-                  >
-                    Sign Out
-                  </Button>
-                </Link>
-              </MenuItem>
+                <MenuItem
+                  _hover={{ bg: "none" }}
+                  _focus={{ bg: "none" }}
+                  color="red.400"
+                  borderRadius="8px"
+                  px="14px"
+                >
+                  <Link w="100%" href="#">
+                    <Button
+                      w="100%"
+                      h="44px"
+                      variant="no-hover"
+                      color={textColor}
+                      bg="transparent"
+                      onClick={handleLogout}
+                    >
+                      Sign Out
+                    </Button>
+                  </Link>
+                </MenuItem>
+              </Flex>
             </Flex>
-          </Flex>
-        </MotionMenuList>
-      </Menu>
-    </Flex>
+          </MotionMenuList>
+        </Menu>
+      </Flex>
+    )
   );
 }
 
