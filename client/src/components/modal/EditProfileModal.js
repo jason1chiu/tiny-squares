f0919a32d9ed1b2440b61dec85616620162f36dc
cleclair71 <cassieleclair71@gmail.com>
2023-08-17 18:06:56 -0400

    edit profile modal edit

--- client/src/components/modal/EditProfileModal.js
+++ client/src/components/modal/EditProfileModal.js
@@ -19,6 +19,7 @@ import {
   Collapse,
   Grid,
   Image,
+  Center
 } from "@chakra-ui/react";
 import { FiUpload } from "react-icons/fi";
 import { useAuth } from "contexts/auth.context";
@@ -78,103 +79,60 @@ function EditProfileModal({ isOpen, onClose, currentUsername }) {
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader color={tColor}>Edit Profile</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <FormControl>
-            <FormLabel color={pColor}>Username</FormLabel>
-            <Input
-              variant="auth"
-              value={username}
-              onChange={handleUsernameChange}
-              placeholder={currentUsername || "Username"}
-            />
-            <FormLabel mt={3} color={pColor}>
-              Select Avatar
-            </FormLabel>
-            <Box>
-              <Image
-                src={avatar ? `/img/avatar/${avatar}.webp` : ""}
-                borderRadius="full"
-                boxSize="100px"
-                objectFit="cover"
-              />
-              <IconButton
-                aria-label="Select avatar"
-                icon={<FiUpload />}
-                onClick={onAvatarToggle}
-              />
-              <Collapse in={isAvatarOpen}>
-                <Grid templateColumns="repeat(3, 1fr)" gap={3}>
-                  {[...Array(18)].map((_, i) => (
-                    <Box
-                      key={i}
-                      onClick={() => handleAvatarChange(i + 1)}
-                      cursor="pointer"
-                      border={avatar === i + 1 ? "2px solid purple.500" : ""}
-                      transition="transform 0.3s"
-                      _hover={{
-                        transform: "scale(1.2)",
-                      }}
-                    >
-                      <Image src={`/img/avatar/${i + 1}.webp`} />
-                    </Box>
-                  ))}
-                </Grid>
-              </Collapse>
+  <ModalOverlay />
+  <ModalContent>
+    <ModalHeader color={tColor}>Edit Profile</ModalHeader>
+    <ModalCloseButton />
+    <ModalBody>
+      <FormControl>
+        <FormLabel color={pColor}>Username</FormLabel>
+        <Input variant="auth" value={username} onChange={handleUsernameChange} placeholder={currentUsername || "Username"} />
+        <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+          <Box>
+          <Center><FormLabel mt={3} color={pColor}>Select Avatar</FormLabel></Center>
+            <Box mx="auto" position="relative" cursor="pointer" onClick={onAvatarToggle}>
+              <Image mx="auto" src={avatar ? `/img/avatar/${avatar}.webp` : ""} borderRadius="full" boxSize="100px" objectFit="cover" />
+              <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" opacity="0" _hover={{ opacity: "1" }} >
+                <FiUpload color="white" size="32"/>
+              </Box>
             </Box>
-            <FormLabel mt={3} color={pColor}>
-              Select Cover Photo
-            </FormLabel>
-            <Box>
-              <Image
-                src={cover ? `/img/cover/${cover}.webp` : ""}
-                borderRadius="full"
-                boxSize="100px"
-                objectFit="cover"
-              />
-              <IconButton
-                aria-label="Select cover photo"
-                icon={<FiUpload />}
-                onClick={onCoverToggle}
-              />
-              <Collapse in={isCoverOpen}>
-                <Grid templateColumns="repeat(3, 1fr)" gap={3}>
-                  {[...Array(10)].map((_, i) => (
-                    <Box
-                      key={i}
-                      onClick={() => handleCoverChange(i + 1)}
-                      cursor="pointer"
-                      border={cover === i + 1 ? "2px solid purple.500" : ""}
-                      transition="transform 0.3s"
-                      _hover={{
-                        transform: "scale(1.2)",
-                      }}
-                    >
-                      <Image src={`/img/cover/${i + 1}.webp`} />
-                    </Box>
-                  ))}
-                </Grid>
-              </Collapse>
+            <Collapse in={isAvatarOpen}>
+              <Grid templateColumns="repeat(3, 1fr)" gap={3}>
+                {[...Array(18)].map((_, i) => (
+                  <Box key={i} onClick={() => handleAvatarChange(i + 1)} cursor="pointer" border={avatar === i + 1 ? "2px solid purple.500" : ""} transition="transform 0.3s" _hover={{ transform: "scale(1.2)" }} >
+                    <Image src={`/img/avatar/${i + 1}.webp`} />
+                  </Box>
+                ))}
+              </Grid>
+            </Collapse>
+          </Box>
+          <Box>
+            <Center><FormLabel mx="auto" mt={3} color={pColor}>Select Cover Photo</FormLabel></Center>
+            <Box position="relative" cursor="pointer" onClick={onCoverToggle}>
+              <Image mt={3.5} mb="15px" mx="auto" src={cover ? `/img/cover/${cover}.webp` : ""} borderRadius="full" boxSize="85px" objectFit="cover" />
+              <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" opacity="0" _hover={{ opacity: "1" }} >
+                <FiUpload color="white" size="32"/>
+              </Box>
             </Box>
-          </FormControl>
-        </ModalBody>
-        <ModalFooter>
-          <Button variant="brand"
-            color="white"
-            fontSize="sm"
-            fontWeight="500"
-             mr={3} onClick={handleSaveProfile}>
-            Save
-          </Button>
-          <Button variant="ghost" onClick={onClose} color={tColor}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+            <Collapse in={isCoverOpen}>
+              <Grid templateColumns="repeat(3, 1fr)" gap={3}>
+                {[...Array(10)].map((_, i) => (
+                  <Box key={i} onClick={() => handleCoverChange(i + 1)} cursor="pointer" border={cover === i + 1 ? "2px solid purple.500" : ""} transition="transform 0.3s" _hover={{ transform: "scale(1.2)" }} >
+                    <Image src={`/img/cover/${i + 1}.webp`} />
+                  </Box>
+                ))}
+              </Grid>
+            </Collapse>
+          </Box>
+        </Grid>
+      </FormControl>
+    </ModalBody>
+    <ModalFooter>
+      <Button variant="brand" color="white" fontSize="sm" fontWeight="500" mr={3} onClick={handleSaveProfile}>Save</Button>
+      <Button variant="ghost" onClick={onClose} color={tColor}>Cancel</Button>
+    </ModalFooter>
+  </ModalContent>
+</Modal>
   );
 }
 
