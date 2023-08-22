d3ae83f21fcc188e31b3577717e7fa4fabf5dee9
Calvin Trieu <calvin.trieu@hotmail.com>
2023-08-22 02:03:53 -0400

    added some security to premium

--- client/src/views/admin/successOrderPage/index.jsx
+++ client/src/views/admin/successOrderPage/index.jsx
@@ -2,26 +2,25 @@ import React, { useEffect } from 'react';
 import { Center, Box } from '@chakra-ui/react';
 import { Link } from "react-router-dom";
 import Confetti from 'react-confetti';
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { UPDATE_USER_PREMIUM_STATUS } from '../../../utils/mutations';
 import { useLocation } from "react-router-dom";
+import { GET_ME } from "utils/queries";
 
 function SuccessPage() {
   const location = useLocation();
+  const {loading, data, error } = useQuery(GET_ME);
   const currentUserId = localStorage.getItem('user_id');
   const urlParams = new URLSearchParams(location.search);
   const userId = urlParams.get("userId");
+  let content;
 
   const [updateUserPremiumStatus] = useMutation(UPDATE_USER_PREMIUM_STATUS);
   
   useEffect(() => {
-    // Extract userId from the query parameter using useLocation()
-
-    console.log(userId);
-    console.log(currentUserId);
 
     if (currentUserId && userId === currentUserId) {
-      // Call the updateUserPremiumStatus mutation
+ 
       updateUserPremiumStatus({
         variables: {
           userId: userId,
@@ -31,10 +30,45 @@ function SuccessPage() {
     }
   }, [updateUserPremiumStatus, currentUserId, userId]);
 
+  if (loading) {
+    return <p>Loading...</p>; // Display a loading indicator if data is being fetched
+  }
 
-  return (
-    <>
-      <Confetti />
+  if (error) {
+    return <p>Error: {error.message}</p>; // Display an error message if there's an error
+  }
+
+
+  if (currentUserId && userId === currentUserId) {
+    content = (
+      <>
+        <Confetti />
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+          fontSize="50px"
+        >
+          <p><Center>Thank You For Your Support, {data?.me?.username}! 🎊<br /></Center> 
+          <p><Center>You now have premium FOREVER!</Center></p>
+
+            <Link to={"/admin/dashboard"}>
+              <Center fontSize="2rem" css={{
+                cursor: 'pointer',
+                transition: 'color 0.3s ease',
+                '&:hover': {
+                  color: 'blue',
+                },
+              }}>Back to Main Page</Center>
+            </Link>
+          </p>
+
+        </Box>
+      </>
+    );
+  } else {
+    content = (
       <Box
         display="flex"
         justifyContent="center"
@@ -42,22 +76,13 @@ function SuccessPage() {
         height="100vh"
         fontSize="50px"
       >
-        <p>Thank You For Your Order! 🎊 <br />
-
-          <Link to={"/admin/dashboard"}>
-            <Center fontSize="2rem" css={{
-              cursor: 'pointer',
-              transition: 'color 0.3s ease',
-              '&:hover': {
-                color: 'blue',
-              },
-            }}>Back to Main Page</Center>
-          </Link>
-        </p>
-
+        <p>Access Denied! You are not authorized to view this page.</p>
+  
       </Box>
-    </>
-  );
+    );
+  }
+
+  return <>{content}</>;
 }
 
 export default SuccessPage;
